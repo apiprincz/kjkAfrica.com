c2ddc505b4bcfd12f1c08c3f98f9b76e838ab49d
Niyi <princzny@gmail.com>
2020-02-13 18:28:36 +0100

    update

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,19 @@
+
+import MobileNavigation from "./modules/MobileNav";
+import ScrollJs from "./modules/Scroll";
+import SwiperSection from "./modules/Swiper";
+import Map from "./modules/Map";
+import ResponsiveMap from "./modules/ResponsiveMap";
+import KeepFormStyleTwo from "./modules/KeepFormStyletwo";
+import SideContent from "./modules/SideContentToggle";
+import HeaderMapAnim from "./modules/HeaderMapAnim";
+
+
+let mobileNavigation = new MobileNavigation();
+let scrollJs = new ScrollJs();
+let swiper = new SwiperSection();
+let map = new Map();
+let responsiveMap = new ResponsiveMap();
+let keepFormStyleTwo = new KeepFormStyleTwo();
+let sideContent = new SideContent();
+let headerMapAnim = new HeaderMapAnim();
